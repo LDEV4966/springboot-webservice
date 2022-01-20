f970d09deb8e7a63af47ec5ce8358bbdee452c84
leeseungmin4966 <bae8416o@naver.com>
2022-01-21 00:28:10 +0900

    Feat : 글 등록 페이지 생성 및 클라이언트 요청을 위한 js 생성

--- src/main/java/com/lsm/test/springboot/web/IndexController.java
+++ src/main/java/com/lsm/test/springboot/web/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index(){
         return "index"; // gradle의 mustache-starter 덕분에 경로설정 및 머스테치 확장자가 자동으로 설정됨
     }
