4791c2044a249573fd0a72c4157b29a953c1281d
leeseungmin4966 <bae8416o@naver.com>
2022-01-22 20:13:03 +0900

    Feat : SpringSecutiry와 Oauth2.0으로 로그인 기능 뷰 구현

--- src/main/java/com/lsm/test/springboot/web/IndexController.java
+++ src/main/java/com/lsm/test/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.lsm.test.springboot.web;
 
+import com.lsm.test.springboot.config.auth.dto.SessionUser;
 import com.lsm.test.springboot.service.posts.PostsService;
 import com.lsm.test.springboot.web.dto.PostsResponseDto;
 import com.lsm.test.springboot.web.dto.PostsUpdateRequestDto;
@@ -9,16 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("posts",postsService.findAllDesc());
-        return "index"; // gradle의 mustache-starter 덕분에 경로설정 및 머스테치 확장자가 자동으로 설정됨
+    public String index(Model model) {
+
+        model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
     }
 
     @GetMapping("/posts/save")
