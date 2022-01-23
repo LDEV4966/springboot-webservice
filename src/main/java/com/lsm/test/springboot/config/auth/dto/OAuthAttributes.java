5a3b9a6ddf97357c871b32a583634fe2b811ad16
leeseungmin4966 <bae8416o@naver.com>
2022-01-23 19:01:00 +0900

    Feat : Naver 로그인 기능 api 추가

--- src/main/java/com/lsm/test/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/lsm/test/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,8 +27,24 @@ public class OAuthAttributes {
 
     //OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야만 합니다.
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String,Object> attributes){
+
+        System.out.println(registrationId + "," + userNameAttributeName + "," + attributes.toString());
+        if("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String,Object> attributes ){
+        Map<String,Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_img"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName) //사실상 naver는 userNameAttributeName이 PK역할을 하지못한다. "response"로 고정시켜놓음.
+                .build();
+    };
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String,Object> attributes){
         return OAuthAttributes.builder()
--- src/main/java/com/lsm/test/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/com/lsm/test/springboot/config/auth/dto/SessionUser.java
@@ -3,15 +3,26 @@ package com.lsm.test.springboot.config.auth.dto;
 import com.lsm.test.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
 
-    public SessionUser(User user){
+    public SessionUser(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
+    }
+
+    @Override
+    public String toString() {
+        return "SessionUser{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", picture='" + picture + '\'' +
+                '}';
     }
 }
