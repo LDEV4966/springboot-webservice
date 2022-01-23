ac313c2f51bec570c81e1474563879d053efaf5e
leeseungmin4966 <bae8416o@naver.com>
2022-01-23 19:28:20 +0900

    Fix : 스프링시큐리티 적용후 에러 수정1
    1. test 환경 분리 : 추가적인 application.properties 추가
    2. 임의의 인증된 사용자를 생성하기 위한 의존성 추가 in gradle

--- src/test/java/com/lsm/test/springboot/web/IndexControllerTest.java
+++ src/test/java/com/lsm/test/springboot/web/IndexControllerTest.java
@@ -1,7 +1,4 @@
 package com.lsm.test.springboot.web;
-
-
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,20 +6,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.swing.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@RunWith(SpringRunner.class) // 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 public class IndexControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void 메인페이지_로딩(){
+    public void 메인페이지_로딩() {
         //when
-        String body = this.restTemplate.getForObject("/",String.class);
+        String body = this.restTemplate.getForObject("/", String.class);
 
-        Assertions.assertThat(body).contains("스프링 부트로 시작하는 웹 서비스"); // 전체를 검사하진 못하고 부분을 포함하는지 검사
+        //then
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
